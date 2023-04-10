0cb3008956e0bec4145c50a8925cec648592c192
Zakjanzi <zakjanzi97@gmail.com>
2023-04-10 16:53:18 +0300

    removed google sheets and added MongoDb

--- app.py
+++ app.py
@@ -1,52 +1,26 @@
 # Flask: A micro web framework for Python used to build web applications.
 # jsonify: A utility function from Flask used to serialize data to JSON format
 from flask import Flask, jsonify
-
 # A Python library used for web scraping to extract data from HTML and XML documents.
 from bs4 import BeautifulSoup
-
 # A Python library used for making HTTP requests.
 import requests
-
-# A Python library used for accessing Google Sheets API
-import gspread
-
-# A module for loading Google Cloud Platform (GCP) credentials, used for authenticating with Google APIs.
-from google.oauth2.service_account import Credentials
-
+# Connecting to MongoDB
+from pymongo import MongoClient
 # A module used to load environment variables from a .env file.
 from dotenv import load_dotenv
-
-# A module used to access operating system functionalities like environment variables
 import os
+# To schedule the scraper
+import schedule
+import time
 
 
 # Load the environment variables from .env file
 load_dotenv()
-
-# Define the credentials for accessing the Google Sheets API
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-
-creds_dict = {
-    "type": os.getenv("TYPE"),
-    "project_id": os.getenv("PROJECT_ID"),
-    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-    "private_key": os.getenv("PRIVATE_KEY").replace('\\n', '\n'),
-    "client_email": os.getenv("CLIENT_EMAIL"),
-    "client_id": os.getenv("CLIENT_ID"),
-    "auth_uri": os.getenv("AUTH_URI"),
-    "token_uri": os.getenv("TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL")
-}
-
-creds = Credentials.from_service_account_info(info=creds_dict, scopes=scope)
-
-client = gspread.authorize(creds)
-spreadsheet = client.create('lira-rate-scraper')
-
-sheet = client.open('lira-rate-scraper').sheet1
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client.test
+usd_lbp_collection = db.usd_lbp
 
 app = Flask(__name__)
 
@@ -58,35 +32,39 @@ def scrape_lira_rate():
     # Scraper setup
     url = 'https://lirarate.org'
     response = requests.get(url)
-    # uncomment only if using proxy:
-    # response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     
     # Extract the buy value of the Lira
     buy_value = soup.find('div', {'class': 'wp-block-column'}).text.strip()
-
     # Extract the sell value of the Lira
-    sell_value = soup.find('div', {'class': 'wp-block-column'}).text.strip()
-
-    # Store the scraped data in a Google Sheet
-    row = [buy_value, sell_value]
-    sheet.insert_row(row, 2)
+    sell_value = soup.find_all('div', {'class': 'wp-block-column'})[1].text.strip()
 
     print(buy_value, sell_value)
 
     # Return a message indicating status
 
     if buy_value and sell_value:
+        usd_lbp_collection.insert_one({
+            'buy_value': buy_value,
+            'sell_value': sell_value
+        })
         return jsonify({'message': 'Scraped and stored the Lira rate successfully'})
     else:
         return jsonify({'message': 'Error: could not scrape the Lira rate'})
 
+def schedule_job():
+    schedule.every(1).minutes.do(scrape_lira_rate)
 
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 if __name__ == '__main__':
+    # Starting the scheduler
+    schedule_job()
+
+    # Starting the Flask app
     app.run(debug=True)
-
-
 
 
 # <p class="has-text-align-center inherit-font" id="latest-buy"><strong id="buy-value" data-value="97500">Buy</strong> 1 USD at 97,500 LBP</p>
